b117328631fb97019b7c6d11571f3ac933e70c86
i1li <74943880+i1li@users.noreply.github.com>
2024-05-05 12:09:15 -0500

    i

--- serv.js
+++ serv.js
@@ -7,6 +7,13 @@ serv.use(express.static(path.join(__dirname)));
 serv.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
+serv.get('*', (req, res) => {
+    if (req.path !== '/index.html') {
+        res.status(404).sendFile(path.join(__dirname, '404.html'));
+    } else {
+        res.sendFile(path.join(__dirname, 'index.html'));
+    }
+});
 serv.listen(PORT, () => {
     console.log(`Server is running on port ${PORT} - \x1b[34mhttp://localhost:${PORT}/\x1b[0m`);
     openBrowser(`http://localhost:${PORT}/`);
