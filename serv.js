abd8fa0c4faa21569d442e3f6072bd626004edd0
i1li <74943880+i1li@users.noreply.github.com>
2024-05-11 15:14:58 -0500

    i

--- serv.js
+++ serv.js
@@ -1,19 +1,11 @@
+const PORT = process.env.PORT || 5000;
 const express = require('express');
 const serv = express();
-const path = require('path');
 const { exec } = require('child_process');
-const PORT = process.env.PORT || 5000;
-serv.use(express.static(path.join(__dirname)));
-serv.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+serv.use(express.static(__dirname));
 serv.get('*', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
     new URL(req.url, `http://${req.headers.host}`);
-    res.sendFile(path.join(__dirname, 'index.html'), (err) => {
-        if (err) {
-            res.status(404).send('404 Not Found');
-        }
-    });
 });
 serv.listen(PORT, () => {
     console.log(`Server is running on port ${PORT} - \x1b[34mhttp://localhost:${PORT}/\x1b[0m`);
