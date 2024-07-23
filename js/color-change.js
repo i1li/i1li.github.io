a57210dac06d2961fe22fa82228d715547e1ffbe
i1li <74943880+i1li@users.noreply.github.com>
2024-07-22 20:47:22 -0500

    i

--- js/color-change.js
+++ js/color-change.js
@@ -1,9 +1,16 @@
+function isDesktop() {
+  const isMobile = (navigator.maxTouchPoints && navigator.maxTouchPoints > 2) ||
+                   (window.matchMedia && window.matchMedia("(max-width: 768px)").matches) ||
+                   /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini|windows phone|kindle|playbook|silk|mobile|tablet|samsung|lg|htc|nokia|motorola|symbian|fennec|maemo|tizen|blazer|series60|ucbrowser|bada|micromessenger|webview/.test(navigator.userAgent.toLowerCase());
+  return !isMobile;
+}
+if (isDesktop()) {
 // Random Color on Hover, Click, or Touch of Links, Buttons + other elements
-const hover = document.querySelectorAll('button, a, a.dark-mode, .article-header-wrapper, footer, .article-nav-bottom');
-// Shift colors of elements in 'alwaysShiftRandomly' at randomly varying rate, regardless of hover.
-const alwaysShiftRandomly = document.querySelectorAll('header, #site-nav .col, #site-nav, .section-nav .col, .article-header, footer, .article-title, #site-title, #light-dark-zoom');
+const hoverShift = document.querySelectorAll('button, a, a.dark-mode, .article-header-wrapper, footer, .article-nav-bottom');
+// Shift colors of elements in 'alwaysShift' at randomly varying rate, regardless of hover.
+const alwaysShift = document.querySelectorAll('header, #site-nav .col, #site-nav, .section-nav .col, .article-header, footer, .article-title, #site-title, #light-dark-zoom');
 // Rotate hue between -315 and 315 degrees, excluding -45 to 45. (Avoids similar hue ranges.)
-function startAlwaysShiftRandomly(element, interval) {
+function startalwaysShift(element, interval) {
   let randomDegree = Math.random() < 0.5 ? Math.floor(Math.random() * -270) - 45 : Math.floor(Math.random() * 270) + 46;
   element.style.filter = `hue-rotate(${randomDegree}deg)`;
   return setInterval(() => {
@@ -11,7 +18,7 @@ function startAlwaysShiftRandomly(element, interval) {
     element.style.filter = `hue-rotate(${randomDegree}deg)`;
   }, interval);
 }
-function startHover(element, interval = 100) {
+function startHoverShift(element, interval = 100) {
   let randomDegree = Math.random() < 0.5 ? Math.floor(Math.random() * -270) - 45 : Math.floor(Math.random() * 270) + 46;
   element.style.filter = `hue-rotate(${randomDegree}deg) drop-shadow(0 0 1em rgba(255, 255, 255, 0.5))`;
   return setInterval(() => {
@@ -23,30 +30,31 @@ function handleDisengage(element, intervalId) {
   clearInterval(intervalId);
   element.style.filter = 'none';
 }
-alwaysShiftRandomly.forEach(element => {
+alwaysShift.forEach(element => {
   element.shiftIncrement = Math.floor(Math.random() * 111) + 1;
   const randomInterval = 111 + Math.floor(Math.random() * 1001);
-  element.intervalId = startAlwaysShiftRandomly(element, randomInterval);
+  element.intervalId = startalwaysShift(element, randomInterval);
   setInterval(() => {
     element.shiftIncrement = Math.random() < 0.5 ? Math.floor(Math.random() * 11) + 1 : Math.floor(Math.random() * -10);
   }, 111 + Math.random() * 1234);
 });
-hover.forEach(element => {
+hoverShift.forEach(element => {
   let intervalId;
   let disengageTimeout;
   element.addEventListener('mouseover', () => {
-    intervalId = startHover(element);
+    intervalId = startHoverShift(element);
   });
   element.addEventListener('click', () => {
     clearInterval(intervalId);
-    intervalId = startHover(element);
+    intervalId = startHoverShift(element);
   });
   element.addEventListener('touchstart', () => {
     clearInterval(intervalId);
-    intervalId = startHover(element);
+    intervalId = startHoverShift(element);
     disengageTimeout = setTimeout(() => handleDisengage(element, intervalId), 888);
   });
   element.addEventListener('mouseout', () => {
     handleDisengage(element, intervalId);
   });
 });
+}
