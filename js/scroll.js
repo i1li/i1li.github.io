d49eebec6b7c90556a0e08dfd46dfc8f90845ad1
i1li <74943880+i1li@users.noreply.github.com>
2024-11-18 13:53:35 -0600

    i

--- js/scroll.js
+++ js/scroll.js
@@ -57,6 +57,10 @@ function throttle(func, limit) {
     const context = this;
     const args = arguments;
     if (!lastRan) {
+      func.apply(context, args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFunc);
       lastFunc = setTimeout(function() {
         if ((Date.now() - lastRan) >= limit) {
           func.apply(context, args);
