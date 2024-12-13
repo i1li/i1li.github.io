629c719cc4d247af9ab5cf949f95bc41248352a9
i1li <74943880+i1li@users.noreply.github.com>
2024-12-13 10:33:02 -0600

    i

--- src/js/bible-verses.js
+++ src/js/bible-verses.js
@@ -1,85 +1,4 @@
-const bibleVerses = [
-{
-    verse: "Exodus 3:14",
-    text: "I AM WHO I AM. This is what you are to say to the Israelites: 'I AM has sent me to you.'"
-},
-{
-    verse: "John 6:35",
-    text: "I am the bread of life. Whoever comes to me will never go hungry, and whoever believes in me will never be thirsty."
-},
-{
-    verse: "John 8:12",
-    text: "I am the light of the world. Whoever follows me will never walk in darkness, but will have the light of life."
-},
-{
-    verse: "John 10:9",
-    text: "I am the gate; whoever enters through me will be saved."
-},
-{
-    verse: "John 10:11",
-    text: "I am the good shepherd. The good shepherd lays down his life for the sheep."
-},
-{
-    verse: "John 11:25-26",
-    text: "I am the resurrection and the life. The one who believes in me will live, even though they die; and whoever lives by believing in me will never die."
-},
-{
-    verse: "John 14:6",
-    text: "I am the way and the truth and the life. No one comes to the Father except through me."
-},
-{
-    verse: "John 15:1",
-    text: "I am the true vine, and my Father is the gardener."
-},
-{
-    verse: "John 8:58",
-    text: "Truly, truly, I say to you, before Abraham was, I am."
-},
-{
-    verse: "Revelation 1:8",
-    text: "I am the Alpha and the Omega, says the Lord God, who is, and who was, and who is to come, the Almighty."
-},
-{
-verse: "Philippians 4:13",
-text: "I can do all things through him who strengthens me."
-},
-{
-verse: "Jeremiah 29:11",
-text: "'For I know the plans I have for you' - this is the Lord's declaration - 'plans for your well-being, not for disaster, to give you a future and a hope.'"
-},
-{
-verse: "Romans 8:28",
-text: "We know that all things work together for the good of those who love God, who are called according to his purpose."
-},
-{
-verse: "Psalm 23:1",
-text: "The Lord is my shepherd; I have what I need."
-},
-{
-verse: "Proverbs 3:5-6",
-text: "Trust in the Lord with all your heart, and do not rely on your own understanding; in all your ways know him, and he will make your paths straight."
-},
-{
-verse: "Isaiah 41:10",
-text: "Do not fear, for I am with you; do not be afraid, for I am your God. I will strengthen you; I will help you; I will hold on to you with my righteous right hand."
-},
-{
-verse: "2 Corinthians 5:17",
-text: "Therefore, if anyone is in Christ, he is a new creation; the old has passed away, and see, the new has come!"
-},
-{
-verse: "Matthew 11:28",
-text: "Come to me, all of you who are weary and burdened, and I will give you rest."
-},
-{
-verse: "Galatians 2:20",
-text: "I have been crucified with Christ, and I no longer live, but Christ lives in me. The life I now live in the body, I live by faith in the Son of God, who loved me and gave himself for me."
-},
-{
-verse: "Joshua 1:9",
-text: "Haven't I commanded you: be strong and courageous? Do not be afraid or discouraged, for the Lord your God is with you wherever you go."
-}
-];
+
 function constructBLBUrl(verse) {
 const [book, chapter_verse] = verse.split(' ');
 const [chapter, verse_range] = chapter_verse.split(':');
@@ -95,14 +14,14 @@ function displayRandomBibleVerse() {
   function displayVerse() {
     const bibleVerse = bibleVerses[currentIndex];
     document.querySelectorAll('.bible-verse-text').forEach(element => {
-      element.textContent = bibleVerse.text;
+      element.textContent = bibleVerse[1];
     });
     document.querySelectorAll('.bible-verse-link').forEach(element => {
-      element.textContent = bibleVerse.verse;
-      element.href = constructBLBUrl(bibleVerse.verse);
+      element.textContent = bibleVerse[0];
+      element.href = constructBLBUrl(bibleVerse[0]);
     });
-    const wordCount = bibleVerse.text.split(' ').length;
-    remainingTime = wordCount * 650;
+    const wordCount = bibleVerse[1].split(' ').length;
+    remainingTime = Math.max(wordCount * 650, 3300);
     currentIndex = (currentIndex + 1) % bibleVerses.length;
     checkAndSetTimeout();
   }
@@ -128,12 +47,64 @@ function displayRandomBibleVerse() {
 }
 const headerVerse = document.getElementById('header-verse');
 document.addEventListener("DOMContentLoaded", function() {
+  if (isMobile) {
+    headerVerse.style.display = 'none';
+  }
   if (isInitialLoad) {
     displayRandomBibleVerse();
     welcomeOverlay.style.opacity = welcomeInitialOpacity;
     requestAnimationFrame((timestamp) => animateOverlay(timestamp));
   }
-  if (isMobile) {
-    headerVerse.style.display = 'none';
-  }
 });
+const bibleVerses = [[
+"Philippians 4:7",
+"And the peace of God, which surpasses all understanding, will guard your hearts and minds in Christ Jesus."
+],[
+"Exodus 3:14",
+"God replied to Moses, 'I AM WHO I AM. This is what you are to say to the Israelites: I AM has sent me to you.'"
+],[
+"John 6:35",
+"I am the bread of life. Whoever comes to me will never go hungry, and whoever believes in me will never be thirsty."
+],[
+"John 8:12",
+"I am the light of the world. Whoever follows me will never walk in darkness, but will have the light of life."
+],[
+"John 10:9",
+"I am the gate; whoever enters through me will be saved."
+],[
+"John 11:25-26",
+"I am the resurrection and the life. The one who believes in me will live, even though they die; and whoever lives by believing in me will never die."
+],[
+"John 14:6",
+"I am the way and the truth and the life. No one comes to the Father except through me."
+],[
+"John 15:1",
+"I am the true vine, and my Father is the gardener."
+],[
+"John 8:58",
+"Truly, truly, I say to you, before Abraham was, I am."
+],[
+"Revelation 1:8",
+"I am the Alpha and the Omega, says the Lord God, who is, and who was, and who is to come, the Almighty."
+],[
+"Proverbs 3:5-6",
+"Trust in the Lord with all your heart, and do not rely on your own understanding; in all your ways know him, and he will make your paths straight."
+],[
+"2 Corinthians 5:17",
+"Therefore, if anyone is in Christ, he is a new creation; the old has passed away, and see, the new has come!"
+],[
+"Matthew 6:24",
+"No one can serve two masters,  since either he will hate one and love the other, or he will be devoted to one and despise the other. You cannot serve both God and money."
+],[
+"2 Timothy 1:7",
+"For God has not given us a spirit of fear, but of power and of love and of a sound mind."
+],[
+"Romans 12:2",
+"Do not be conformed to this age, but be transformed by the renewing of your mind, so that you may discern what is the good, pleasing, and perfect will of God."
+],[
+"Proverbs 9:8-9",
+"Do not rebuke mockers or they will hate you; rebuke the wise and they will love you. Instruct the wise and they will be wiser still; teach the righteous and they will add to their learning."
+],[
+"Galatians 2:20",
+"I have been crucified with Christ, and I no longer live, but Christ lives in me. The life I now live in the body, I live by faith in the Son of God, who loved me and gave himself for me."
+]];
