d488197d89db07fb66bd1eb5f9c83442b71d58f8
i1li <74943880+i1li@users.noreply.github.com>
2025-11-26 21:35:27 -0600

    i

--- src/js/github-pages.js
+++ src/js/github-pages.js
@@ -11,9 +11,10 @@
 // the single page app to route accordingly.
 
 // Updated to modern syntax in 2024 for https://github.com/i1li/i
-if (window.SPA404Redirect) {
-  const decoded = window.SPA404Redirect.search.slice(1).split('&').map(s => s.replace(/~and~/g, '&')).join('?');
-  const newUrl = window.SPA404Redirect.origin + window.SPA404Redirect.pathname.slice(0, -1) + decoded + window.SPA404Redirect.hash;
-  window.history.replaceState(null, null, newUrl);
-  delete window.SPA404Redirect;
-}
+(function (l) {
+  if (l.search.startsWith('?/')) {
+    const decoded = l.search.slice(1).split('&').map(s => s.replace(/~and~/g, '&')).join('?');
+    const newUrl = l.origin + l.pathname.slice(0, -1) + decoded + l.hash;
+    window.history.replaceState(null, null, newUrl);
+  }
+})(window.location);
